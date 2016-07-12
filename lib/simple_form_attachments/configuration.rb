d28b2f973fdd7407b4b6ecee045a21c8b2762174
Tomas Celizna <tomas.celizna@gmail.com>
2016-07-12 15:46:38 +0200

    cleanup

--- lib/simple_form_attachments/configuration.rb
+++ lib/simple_form_attachments/configuration.rb
@@ -1,11 +1,9 @@
 module SimpleFormAttachments
   class Configuration
-
     attr_accessor :route
 
     def route
       @route ||= SimpleFormAttachments::Engine.routes.url_helpers.attachments_path
     end
-
   end
 end
